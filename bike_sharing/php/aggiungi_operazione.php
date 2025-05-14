a2d8a0774789c1312b6d0efaf8492fc092d5c43b
BenedettaBe <144808596+BenedettaBe@users.noreply.github.com>
2025-05-14 21:44:05 +0200

    upload file corretti

--- bike_sharing/php/aggiungi_operazione.php
+++ bike_sharing/php/aggiungi_operazione.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require("../conf/db_config.php");
 
 if (!isset($_SESSION['login']) || ($_SESSION['tipo'] != 'A')) {
@@ -6,28 +7,28 @@ if (!isset($_SESSION['login']) || ($_SESSION['tipo'] != 'A')) {
     exit();
 }
 
-$stmt = $conn->prepare("SELECT id_utente FROM utenti WHERE tessera=?");
-$stmt->bind_param($_POST["n_tessara"]);
+$stmt = $conn->prepare("SELECT id_utente FROM utenti WHERE tessera=? AND statoUtente='A'");
+$stmt->bind_param("s",$_POST["n_tessera"]);
 $stmt->execute();
 $result = $stmt->get_result();
-$idUtente = $result->fetchone();
+$rowUtente = $result->fetch_assoc();
 
-$stmt = $conn->prepare("SELECT id_bici FROM bici WHERE tag=?");
-$stmt->bind_param($_POST["tag"]);
+$stmt = $conn->prepare("SELECT id_bici FROM biciclette WHERE tag=?");
+$stmt->bind_param("s",$_POST["tag"]);
 $stmt->execute();
 $result = $stmt->get_result();
-$idBici = $result->fetchone();
-if($idBici && $idUtente){
+$rowBici = $result->fetch_assoc();
+if($rowBici && $rowUtente){
     
         $stmt = $conn->prepare("INSERT INTO operazioni(data_ora, tipo, id_utente, id_bici, id_stazione) VALUES (?,?,?,?,?)");
-        $stmt->bind_param(date('Y-m-d H:i:s'), $_POST["operazione"],$idBici,$idUtente,$_POST["id_stazione"]);
+        $stmt->bind_param("ssiii", date('Y-m-d H:i:s'), $_POST["operazione"], $rowUtente["id_utente"],$rowBici["id_bici"],$_POST["id_stazione"]);
         $stmt->execute();
     
     $conn->close();
-    header("Location: ../templates/aggiungi_operazioni.php?msg=OK");
+    header("Location: ../aggiungi_operazione_form.php?msg=OK");
 }
 else{
     $conn->close();
-    header("Location: ../templates/aggiungi_operazioni.php?msg=NO_UTENTE_o_bici");
+    header("Location: ../aggiungi_operazione_form.php?msg=NO_UTENTE_o_bici");
 } 
 ?>
